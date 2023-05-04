9a1bab7ab19652df8d68711bcf79da83c2fcc897
Azzinnur <hermin.chequetet@gmail.com>
2023-05-04 20:50:19 +0300

    Completed 04/05/2023

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,31 +1,16 @@
 package web.controller;
 
-import model.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import service.CarService;
-import service.CarServiceImpl;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class CarController {
     @GetMapping(value = "/cars")
-    public String printCars(ModelMap model, HttpServletRequest request) {
-        int count = 5;
-        List<Car> cars;
-        CarService service = new CarServiceImpl();
-        try{
-            count = Integer.parseInt(request.getParameter("count"));
-        } catch (NumberFormatException exc) {}
-        if ((count >=1) && (count <=4)) {
-            cars = service.printCarTable(count);
-        } else {
-            cars = service.printCarTable(5);
-        }
-        model.addAttribute("cars", cars);
+    public String printCars(@RequestParam(required = false) Integer count, ModelMap model) {
+        model.addAttribute("cars", CarService.printCarTable(count));
         return "cars";
     }
 }
